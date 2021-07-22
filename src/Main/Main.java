c65e6e47747bfc28a9f49546304b5b7ec2b603bf
Roman Kazak <kazakrs@gmail.com>
2021-07-22 14:48:00 +0700

    add some changes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        NewApi newApi = new NewApi();
+        System.out.println(newApi.api);
+
         System.out.println("Hello World!");
+
     }
 }
